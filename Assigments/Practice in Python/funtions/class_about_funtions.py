6563ce4cd2f6571bac7d391dc71de10aed6a26ae
EmilioHermosa <emiliogatop2808@gmail.com>
2019-10-01 22:54:23 -0500

    funtions_in_python

---
+++ Assigments/Practice in Python/funtions/class_about_funtions.py
@@ -0,0 +1,18 @@
+def square_number(number):
+    return number**2
+
+def print_number_pairs():
+    numbers = range(20)
+    even = [square_number(number) for number in numbers if number % 2 == 0]
+    odd = [square_number(number) for number in numbers if number % 2 != 0]
+
+#even = []
+#for number in numbers:
+#    if number % 2 == 0:
+#        even.append(number)
+
+    for even, odd in zip(even, odd):
+        print("{} -> {}".format(even, odd))
+
+
+print_number_pairs()
