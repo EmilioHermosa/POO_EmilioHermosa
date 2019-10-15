d4584834a298be943b0f250896af79e2fb5ee103
EmilioHermosa <emiliogatop2808@gmail.com>
2019-10-15 11:35:00 -0500

    ¨workshop¨

---
+++ Assigments/Practice in Python/examples/ejemplo JSON.py
@@ -0,0 +1,28 @@
+student = {
+    ¨name¨ : ¨Emilio¨
+    ¨subjects¨ : [
+        {
+            ¨name¨ : ¨Arquitectura de Software¨,
+            ¨nrc¨: ¨2018¨
+        },
+        {
+            ¨name¨ : ¨Programación Avanzada¨,
+            ¨nrc¨: ¨5060¨
+        }
+    ],
+    ¨university¨: {
+        ¨name¨: ¨ESPE¨,
+        ¨careers¨: [
+            ¨Sistemas¨,
+            ¨Software¨,
+            ¨TI¨    
+        ]
+    }
+
+}
+
+def main_function(number):
+    def square_number(number):
+        return number ** 2
+
+    return square_number(number)
