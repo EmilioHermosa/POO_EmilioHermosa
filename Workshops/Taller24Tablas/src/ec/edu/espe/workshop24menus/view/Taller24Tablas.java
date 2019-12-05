23bcd61772f154a0fb15de5245466401a0831691
DarianMichael <56969322+DarianMichael@users.noreply.github.com>
2019-12-05 10:27:55 -0500

    Menus

---
+++ Workshops/Taller24Tablas/src/ec/edu/espe/workshop24menus/view/Taller24Tablas.java
@@ -0,0 +1,10 @@
+
+package ec.edu.espe.workshop24menus.view;
+
+public class Taller24Tablas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
